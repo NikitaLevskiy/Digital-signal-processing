198bf5fa16c1701feacd81bcb0f5598869431180
Mykyta Levskyi <nikita.levskiy@gmail.com>
2026-05-10 12:20:46 +0200

    integrator initial commit

---
+++ CIC/CIC_001/Verilog/integrator.v
@@ -0,0 +1,48 @@
+module integrator
+#(
+	parameter DATA_WIDTH = 1,
+	          M = 1
+)(
+	 input wire                  rst_i,
+	 input wire                  clk_i,
+	 input wire [DATA_WIDTH-1:0] data_i,
+	output wire [DATA_WIDTH-1:0] data_o
+);
+
+	wire [DATA_WIDTH-1:0] adder;
+	 reg [DATA_WIDTH-1:0] adder_ff [M-1:0];
+	
+	
+	assign adder = data_i + data_o;
+	assign data_o = adder_ff[M-1];
+
+
+	genvar i;
+	
+	generate
+	
+		for (i = 0; i < M; i = i + 1) begin
+		
+			always @(posedge clk_i) begin
+			
+				if (~rst_i) begin
+				
+					adder_ff[i] <= 'b0;
+				
+				end else if (i == 0) begin
+				
+					adder_ff[i] <= adder;
+				
+				end else begin
+				
+					adder_ff[i] <= adder_ff[i-1];
+				
+				end
+			
+			end
+		
+		end
+	
+	endgenerate
+
+endmodule
